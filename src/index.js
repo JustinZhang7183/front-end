c40b798483e4975387cf010717bccd5df2ec4dab
zhangjunqiang <zhangjunqiang7183@126.com>
2023-04-07 17:26:37 +0800

    add oauth2 flow, but there is a problem: sessionStorage data lost

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import IndexInfo from './indexInfo';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<IndexInfo />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
